983ae29b30fb20fa6e3cac20fab642414b4a355e
amireshoon <abtin.yas53@gmail.com>
2021-05-16 15:25:19 +0430

    Some improvements

--- social-regex.py
+++ social-regex.py
@@ -4,22 +4,19 @@ import json
 f = open("regexes.json", "r")
 regexes = json.loads(f.read())
 
-for regex in regexes:
-    print("Testing ", regex['title'])
+for ir, regex in enumerate(regexes):
+    print("-Testing ", regex['title'])
     totalTests = len(regex['tests'])
+    failedTests = 0
+
     for i, test in enumerate(regex['tests']):
-        print("Test", i+1, "of", totalTests)
+        print("--Test", i+1, "of", totalTests)
         r,c = regex['regex'], test['case']
         x = re.search(r, c)
-        if test['want'] == True:
-            if x == None:
-                print("failed")
-                print("Want:", test['want'], "Got:", False if x == None else True)
-            else:
-                print("pass")
+        if test['want'] == True and x == None or test['want'] == False and x != None:
+            failedTests = failedTests + 1
+            print('--failed')
+            print("--Want:", test['want'], "Got:", False if x == None else True)
         else:
-            if x != None:
-                print("failed")
-                print("Want:", test['want'], "Got:", False if x == None else True)
-            else:
-                print("pass")
+            print('--pass')
+    print("--Tests completed, Passed Tests:", totalTests - failedTests, ",Failed Tests:",failedTests)
